66472916daf7f65ca121b6a09de152eb4cfd7f0b
egorbarinov <egor.barinov@gmail.com>
2021-02-21 01:44:35 +0300

    added code

---
+++ lesson06/demo-app/src/main/java/com/egorbarinov/clientserver/demoapp/DemoAppApplication.java
@@ -0,0 +1,19 @@
+package com.egorbarinov.clientserver.demoapp;
+
+import com.egorbarinov.clientserver.demoapp.repository.PersonRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoAppApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoAppApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		PersonRepository.initDataBase();
+	}
+}
